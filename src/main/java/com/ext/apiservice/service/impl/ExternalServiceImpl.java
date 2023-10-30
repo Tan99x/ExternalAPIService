325b78b2273bb3792f0e9babccd8205c945379ed
Tanveer Shah <tanveer.shah@delaplex.com>
2023-10-30 15:12:56 +0530

    updated

--- src/main/java/com/ext/apiservice/service/impl/ExternalServiceImpl.java
+++ src/main/java/com/ext/apiservice/service/impl/ExternalServiceImpl.java
@@ -1,12 +1,14 @@
 package com.ext.apiservice.service.impl;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ext.apiservice.configutation.ConfigProperties;
 import com.ext.apiservice.service.ExternalService;
 import com.ext.apiservice.service.modal.AutheriseTxnRequestDTO;
 import com.ext.apiservice.service.modal.CardInfoRequest;
@@ -26,57 +28,62 @@ public class ExternalServiceImpl implements ExternalService {
 
 	@Autowired
 	HttpConnector httpConnector;
-	
-	@Autowired
-	ConfigProperties configProperties;
 
 	@Override
 	public String processCard(CardInfoRequest cardInfoRequest, HttpServletRequest request,
 			HttpServletResponse response) {
 		Gson gson = new Gson();
-		String sessionKeyApiUrl = configProperties.getSessionKeyApiUrl();
-		String cardIdentifierApiUrl = "https://pi-test.sagepay.com/api/v1/card-identifiers";
-		String cardTxnApiUrl = "https://pi-test.sagepay.com/api/v1/card-identifiers";
-		String authorizeTxnApiUrl = "https://pi-test.sagepay.com/api/v1/card-identifiers";
+		Properties prop = new Properties();
 
-		MerchantKeyRequest merchantKeyRequest = new MerchantKeyRequest();
-		Map<String, String> header = new HashMap<>();
-		header.put("Content-Type", "application/json");
-		String auth = "Bearer "
-				+ "aEpZeHN3N0hMYmo0MGNCOHVkRVM4Q0RSRkxodUo4RzU0TzZyRHBVWHZFNmhZRHJyaWE6bzJpSFNyRnliWU1acG1XT1FNdWhzWFA1MlY0ZkJ0cHVTRHNocktEU1dzQlkxT2lONmh3ZDlLYjEyejRqNVVzNXU=";
-		header.put("Authorization", auth);
-		merchantKeyRequest.setVendorName("sandbox");
-		String body = CommonUtils.dumpObject(merchantKeyRequest);
-		HttpConnectorResponse httpResponse = httpConnector.postApiCall(sessionKeyApiUrl, header, body);
-		if (HttpConnector.isResponseExist(httpResponse)) {
-			MerchantKeyResponse res = gson.fromJson(httpResponse.getResponse(), MerchantKeyResponse.class);
-			System.out.println(res.getExpiry());
-			System.out.println(res.getMerchantSessionKey());
+		InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties");
+		try {
+			prop.load(stream);
+			String sessionKeyApiUrl = prop.getProperty("sessionKeyApiUrl");
+			String cardIdentifierApiUrl = prop.getProperty("cardIdentifierApiUrl");
+			String cardTxnApiUrl = prop.getProperty("cardTxnApiUrl");
 
-			String reqBody = CommonUtils.dumpObject(cardInfoRequest);
-
-			auth = "Bearer " + res.getMerchantSessionKey();
-
-			Map<String, String> headerMap = new HashMap<>();
+			MerchantKeyRequest merchantKeyRequest = new MerchantKeyRequest();
+			Map<String, String> header = new HashMap<>();
+			header.put("Content-Type", "application/json");
+			String auth = "Basic " + prop.getProperty("authToken");
 			header.put("Authorization", auth);
-
-			HttpConnectorResponse cardResp = httpConnector.postApiCall(cardIdentifierApiUrl, headerMap, reqBody);
+			merchantKeyRequest.setVendorName(prop.getProperty("vendorType"));
+			String body = CommonUtils.dumpObject(merchantKeyRequest);
+			HttpConnectorResponse httpResponse = httpConnector.postApiCall(sessionKeyApiUrl, header, body);
 			if (HttpConnector.isResponseExist(httpResponse)) {
-				cardResp.getResponse();
+				MerchantKeyResponse res = gson.fromJson(httpResponse.getResponse(), MerchantKeyResponse.class);
+				System.out.println(res.getExpiry());
+				System.out.println(res.getMerchantSessionKey());
 
-				TransactionRequestDTO transactionRequestDTO = new TransactionRequestDTO();
-				String txnReqBody = CommonUtils.dumpObject(transactionRequestDTO);
-				HttpConnectorResponse txnResp = httpConnector.postApiCall(cardTxnApiUrl, headerMap, txnReqBody);
-				if (HttpConnector.isResponseExist(txnResp)) {
-					AutheriseTxnRequestDTO autheriseTxnRequestDTO = new AutheriseTxnRequestDTO();
-					String authReqBody = CommonUtils.dumpObject(autheriseTxnRequestDTO);
-					HttpConnectorResponse authResp = httpConnector.postApiCall(authorizeTxnApiUrl, headerMap,
-							authReqBody);
-					if (HttpConnector.isResponseExist(authResp)) {
+				String reqBody = CommonUtils.dumpObject(cardInfoRequest);
 
+				auth = "Bearer " + res.getMerchantSessionKey();
+
+				Map<String, String> headerMap = new HashMap<>();
+				header.put("Authorization", auth);
+
+				HttpConnectorResponse cardResp = httpConnector.postApiCall(cardIdentifierApiUrl, headerMap, reqBody);
+				if (HttpConnector.isResponseExist(httpResponse)) {
+					cardResp.getResponse();
+
+					TransactionRequestDTO transactionRequestDTO = new TransactionRequestDTO();
+					String txnReqBody = CommonUtils.dumpObject(transactionRequestDTO);
+					HttpConnectorResponse txnResp = httpConnector.postApiCall(cardTxnApiUrl, headerMap, txnReqBody);
+					if (HttpConnector.isResponseExist(txnResp)) {
+						AutheriseTxnRequestDTO autheriseTxnRequestDTO = new AutheriseTxnRequestDTO();
+						String authReqBody = CommonUtils.dumpObject(autheriseTxnRequestDTO);
+						HttpConnectorResponse authResp = httpConnector.postApiCall(cardTxnApiUrl, headerMap,
+								authReqBody);
+						if (HttpConnector.isResponseExist(authResp)) {
+
+						}
 					}
 				}
 			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
 		}
 		return null;
 	}
